7a54d1d63cf46c36819552aa1c3b46bf1cf5b2b9
cmchap <mxchap@gmail.com>
2017-10-01 08:58:31 -0700

    cleanup of grid search code

--- src/srcGridSearch/runGridSearch.sh
+++ src/srcGridSearch/runGridSearch.sh
@@ -7,21 +7,20 @@
 
 
 # Grab the number of files
-numfiles=`wc -l < ../srcConfGen/prodConf/config.list`
-
+numfiles=`wc -l < ../srcConfigGen/prodConf/config.list`
+echo ${numfiles}
 
 # Loop over files
 i=1
 while [ $i -le $numfiles ]; do
-    filename=`sed "${i}q;d" ../confGen/config.list`
+    filename=`sed "${i}q;d" ../srcConfigGen/prodConf/config.list`
 
     # Loop over record size (10^j)
     for j in `seq 1 4`; do
 	recsize=$((10**j))
-	echo "python testSystem.py ../srcConfGen/prodConf/${filename} ${recsize}"
+	echo "python testSystem.py ../srcConfigGen/prodConf/${filename} ${recsize}"
 	python testSystem.py ../srcConfGen/prodConf/${filename} ${recsize}
     done
 
     i=$(($i+1))
 done
-
